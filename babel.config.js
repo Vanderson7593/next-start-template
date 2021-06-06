6efac68e1c24ae05f0e210231dc7beedededa567
Vanderson Telema <vandelson14@hotmail.com>
2021-06-06 17:19:49 +0100

    chore: rm babel unnecessary code

--- babel.config.js
+++ babel.config.js
@@ -7,25 +7,5 @@ module.exports = {
     plugins: [
         ['@emotion'],
         ["inline-react-svg"],
-        [
-            'babel-plugin-import',
-            {
-                libraryName: '@material-ui/core',
-                // Use ""libraryDirectory": ""," if your bundler does not support ES modules
-                libraryDirectory: '',
-                camel2DashComponentName: false,
-            },
-            'core',
-        ],
-        [
-            'babel-plugin-import',
-            {
-                libraryName: '@material-ui/icons',
-                // Use ""libraryDirectory": ""," if your bundler does not support ES modules
-                libraryDirectory: '',
-                camel2DashComponentName: false,
-            },
-            'icons',
-        ],
     ],
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-import { colors } from '@material-ui/core'
+import { blue, lightBlue, grey } from '@material-ui/core/colors'
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
 
 let theme = createMuiTheme({
@@ -12,17 +12,17 @@ let theme = createMuiTheme({
   palette: {
     type: 'light',
     background: {
-      default: colors.grey[200],
-      paper: colors.grey[50]
+      default: grey[200],
+      paper: grey[50]
     },
     primary: {
-      light: colors.lightBlue[600],
-      main: colors.blue[700],
-      dark: colors.blue[900]
+      light: lightBlue[600],
+      main: blue[700],
+      dark: blue[900]
     },
     text: {
-      primary: colors.grey[800],
-      secondary: colors.grey[50]
+      primary: grey[800],
+      secondary: grey[50]
     }
   },
   typography: {
